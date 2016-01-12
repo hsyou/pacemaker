bdbb47bf450059092b24601cc76d6f4aea9b857d
hsyou <realhs01@gmail.com>
2016-01-12 21:01:50 +0900

    2

--- app/src/main/java/com/example/hs/pacemaker/main/MainActivity.java
+++ app/src/main/java/com/example/hs/pacemaker/main/MainActivity.java
@@ -7,7 +7,9 @@ import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Toast;
 
 
 import com.example.hs.pacemaker.R;
@@ -42,24 +44,24 @@ public class MainActivity extends AppCompatActivity {
                 mDrawerLayout.closeDrawers();
 
                 if (menuItem.getItemId() == R.id.nav_item_profile) {
-                    FragmentTransaction fragmentTransaction = mFragmentManager.beginTransaction();
-                    fragmentTransaction.replace(R.id.containerView,new ProfileFragment()).commit();
+                    FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
+                    xfragmentTransaction.replace(R.id.containerView, new ProfileFragment()).commit();
                 }
                 if (menuItem.getItemId() == R.id.nav_item_timeline) {
                     FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
-                    xfragmentTransaction.replace(R.id.containerView,new TabFragment()).commit();
+                    xfragmentTransaction.replace(R.id.containerView, new TabFragment()).commit();
                 }
                 if (menuItem.getItemId() == R.id.nav_item_timer) {
                     FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
-                    xfragmentTransaction.replace(R.id.containerView,new TimerTab()).commit();
+                    xfragmentTransaction.replace(R.id.containerView, new TimerTab()).commit();
                 }
                 if (menuItem.getItemId() == R.id.nav_item_planer) {
                     FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
-                    xfragmentTransaction.replace(R.id.containerView,new PlanerTab()).commit();
+                    xfragmentTransaction.replace(R.id.containerView, new PlanerTab()).commit();
                 }
                 if (menuItem.getItemId() == R.id.nav_item_studygroup) {
                     FragmentTransaction xfragmentTransaction = mFragmentManager.beginTransaction();
-                    xfragmentTransaction.replace(R.id.containerView,new StudygroupTab()).commit();
+                    xfragmentTransaction.replace(R.id.containerView, new StudygroupTab()).commit();
                 }
 
                 return false;
@@ -69,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
 
 //툴바
         android.support.v7.widget.Toolbar toolbar = (android.support.v7.widget.Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
         ActionBarDrawerToggle mDrawerToggle = new ActionBarDrawerToggle(this,mDrawerLayout, toolbar,R.string.app_name,
                 R.string.app_name);
         mDrawerLayout.setDrawerListener(mDrawerToggle);
@@ -76,5 +79,28 @@ public class MainActivity extends AppCompatActivity {
         mDrawerToggle.syncState();
 
     }
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
 
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.toolbar_search) {
+            Toast.makeText(MainActivity.this, "search", Toast.LENGTH_SHORT).show();
+        }else if(id==R.id.toolbar_mail){
+
+            Toast.makeText(MainActivity.this, "mail", Toast.LENGTH_SHORT).show();
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
+
